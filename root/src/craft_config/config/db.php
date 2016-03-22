c4b0fd36945090408ced92b71a89cffff42ceb23
darith27 <darith2000@yahoo.com>
2016-03-22 10:29:01 -0500

    multi env db config
    multi env db config

--- root/src/craft_config/config/db.php
+++ root/src/craft_config/config/db.php
@@ -8,20 +8,40 @@
  */
 
 return array(
-
-	// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
-	'server' => 'localhost',
-
-	// The database username to connect with.
-	'user' => 'root',
-
-	// The database password to connect with.
-	'password' => 'vagrant',
-
-	// The name of the database to select.
-	'database' => 'vagrant',
-
-	// The prefix to use when naming tables. This can be no more than 5 characters.
-	'tablePrefix' => 'craft',
-
+    '*' => array(
+			// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+			'server' => 'localhost',
+			// The database username to connect with.
+			'user' => 'UPDATEUSERNAME',
+			// The database password to connect with.
+			'password' => 'UPDATEPASSWORD',
+			// The name of the database to select.
+			'database' => 'PROJECT_cmsdb_prod',
+			// The prefix to use when naming tables. This can be no more than 5 characters.
+			'tablePrefix' => 'craft',
+    ),
+    '.dev' => array(
+			// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+			'server' => 'localhost',
+			// The database username to connect with.
+			'user' => 'root',
+			// The database password to connect with.
+			'password' => 'vagrant',
+			// The name of the database to select.
+			'database' => 'vagrant',
+			// The prefix to use when naming tables. This can be no more than 5 characters.
+			'tablePrefix' => 'craft',
+    ),
+    'stage.' => array(
+			// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
+			'server' => 'localhost',
+			// The database username to connect with.
+			'user' => 'UPDATEUSERNAME',
+			// The database password to connect with.
+			'password' => 'UPDATEPASSWORD',
+			// The name of the database to select.
+			'database' => 'PROJECT_cmsdb_stage',
+			// The prefix to use when naming tables. This can be no more than 5 characters.
+			'tablePrefix' => 'craft',
+    ),
 );

c28d4ba0f7605bbbd669c58232a2d462b9626880
spAnser <spenson66@gmail.com>
2017-05-04 15:19:17 -0500

    Repair double stage. inside default craft/config/db.php.

--- root/src/craft/config/db.php
+++ root/src/craft/config/db.php
@@ -32,7 +32,7 @@ return array(
 			// The prefix to use when naming tables. This can be no more than 5 characters.
 			'tablePrefix' => 'craft',
     ),
-    'stage.' => array(
+    'dev.' => array(
 			// The database server name or IP address. Usually this is 'localhost' or '127.0.0.1'.
 			'server' => '127.0.0.1',
 			// The database username to connect with.
